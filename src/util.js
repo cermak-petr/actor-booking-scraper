0a003db7b26963f7727549d9db70990c61998cc6
Petr Čermák <30902390+cermak-petr@users.noreply.github.com>
2019-07-14 18:46:50 +0200

    Update util.js

--- src/util.js
+++ src/util.js
@@ -79,7 +79,7 @@ module.exports.getWorkingBrowser = async (startUrl, input) => {
         const browser = await Apify.launchPuppeteer(input.proxyConfig || {});
         const page = await browser.newPage();
         try{
-            //await Apify.utils.puppeteer.hideWebDriver(page);
+            await Apify.utils.puppeteer.hideWebDriver(page);
             await page.goto(startUrl, { timeout: 100000 });
             await page.waitForNavigation({ timeout: 100000 });
         } catch(e) {
